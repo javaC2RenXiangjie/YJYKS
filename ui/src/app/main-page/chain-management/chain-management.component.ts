4f0f8846aa88c16ea339b03d4970da62d253a11f
javaC2RenXiangJie <15002136809@163.com>
2019-05-30 03:53:38 +0800

    xx

--- ui/src/app/main-page/chain-management/chain-management.component.ts
+++ ui/src/app/main-page/chain-management/chain-management.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChainManagementComponent implements OnInit {
 
+  mediaElement = document.getElementById('mediaElementID');
+
+  closeVideo() {
+    console.log('xxxxx');
+  }
   constructor() { }
 
   ngOnInit() {
